c70c4ec7fbce0e2f305aed5c46054457f0a1b87b
hosung77 <ho4728@gmail.com>
2025-01-10 14:06:14 +0900

    에러수정
    에러수정

--- P3/src/main/java/com/github/p3/exception/ErrorCode.java
+++ P3/src/main/java/com/github/p3/exception/ErrorCode.java
@@ -49,7 +49,7 @@ public enum ErrorCode {
     SENDER_NOT_FOUND(HttpStatus.NOT_FOUND, "발신자를 찾을 수 없습니다"),
     RECEIVER_NOT_FOUND(HttpStatus.NOT_FOUND, "수신자를 찾을 수 없습니다."),
 
-    CHAT_NOT_FOUND(HttpStatus.NOT_FOUND, "채팅 메시지를 찾을 수 없습니다.");
+    CHAT_NOT_FOUND(HttpStatus.NOT_FOUND, "채팅 메시지를 찾을 수 없습니다."),
 
     // 거래
     TRANSACTION_NOT_FOUND(HttpStatus.NOT_FOUND,"거래중이 아니거나 거래가 완료된 상품입니다.");
